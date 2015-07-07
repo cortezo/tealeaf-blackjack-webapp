1a0d5898b5c46ec343d2fa4d2619dd6b3e44b993
Victor <vwalldev@gmail.com>
2015-07-06 19:49:16 -0700

    Initial game setup

--- main.rb
+++ main.rb
@@ -6,23 +6,66 @@ use Rack::Session::Cookie,  :key => 'rack.session',
                             :secret => 'much_secret_very_wow'
 
 get '/' do
-  if session['username']
+  if session[:player_name]
     redirect :game
   else
+    session[:initialized] = false
     redirect :set_player
   end
 end
 
 get '/game' do
-  @username = session['username']
+
+  if session[:initialized] == false
+    # create a deck and put it in the session
+    suits = ['H', 'D', 'C', 'S']
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    session[:deck] = suits.product(values).shuffle! #[ ['H', '2'], ['H', '3'], ...etc]
+    
+    # deal cards
+      #dealer cards, player cards
+    session[:dealer_cards] = []
+    session[:player_cards] = []
+    session[:dealer_cards] << session[:deck].pop
+    session[:player_cards] << session[:deck].pop
+    session[:dealer_cards] << session[:deck].pop
+    session[:player_cards] << session[:deck].pop
+
+    session[:initialized] = true
+  end
+
+  if session[:player_hit?] == true
+    session[:player_cards] << session[:deck].pop
+    session[:player_hit?] = false
+    redirect '/game'
+  elsif session[:player_stand?] == true
+
+  else
+
+  end
+
   erb :game
 end
 
 get '/set_player' do
-  erb :set_user_form
+  erb :new_user_form
 end
 
-post '/set_username' do
-  session['username'] = params['username']
+post '/new_player' do
+  session[:player_name] = params[:player_name]
   redirect :game
+end
+
+get '/bet' do
+  @player_name = session[:player_name]
+end
+
+post '/hit' do
+  session[:player_hit?] = true
+  redirect '/game'
+end
+
+post '/stand' do
+  session[:player_stand?] = true
+  redirect '/game'
 end
