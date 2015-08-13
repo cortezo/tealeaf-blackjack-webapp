eab96e6017e610f3a17a5fd8917fdcfcc0bd2366
Victor W <vwalldev@gmail.com>
2015-08-13 15:56:35 -0700

    Refinement after viewing final solutions

--- main.rb
+++ main.rb
@@ -5,6 +5,9 @@ use Rack::Session::Cookie,  :key => 'rack.session',
                             :path => '/',
                             :secret => 'much_secret_very_wow'
 
+BLACKJACK_AMOUNT = 21
+DEALER_MINIMUM = 17
+
 #####################################################
 ################### Helper Methods ##################
 #####################################################
@@ -22,10 +25,10 @@ helpers do
     end
 
     # Reduce ace value if total is greater than 21
-    if card_values.reduce(:+) > 21 && card_values.include?(11)
+    if card_values.reduce(:+) > BLACKJACK_AMOUNT && card_values.include?(11)
       loop do
         card_values[card_values.find_index(11)] = 1
-        break if card_values.reduce(:+) <= 21 || !card_values.include?(11)
+        break if card_values.reduce(:+) <= BLACKJACK_AMOUNT || !card_values.include?(11)
       end
     end
     card_values.reduce(:+)
@@ -50,10 +53,13 @@ helpers do
       when /\d/ then card[1].to_s
     end
 
-    "<img class='card' src='images/cards/#{suit}_#{rank}.jpg'>"
+    "<img class='card' src='/images/cards/#{suit}_#{rank}.jpg'>"
   end
 
   def get_winner_message
+    @show_hit_and_stand_buttons = false
+    @show_dealer_hit_button = false
+
     player_score = hand_value(session[:player_cards])
     dealer_score = hand_value(session[:dealer_cards])
 
@@ -62,57 +68,41 @@ helpers do
     @success = nil
     @info = nil
 
-    if session[:player_bust?]
-      @error = "#{session[:player_name]} has busted.  You lose!"
-    elsif session[:dealer_bust?]
-      @success = "Dealer has busted.  You win!"
+    if player_score > BLACKJACK_AMOUNT
+      @error = "#{session[:player_name]} has busted with #{player_score}.  You lose!"
+    elsif dealer_score > BLACKJACK_AMOUNT
+      @success = "Dealer has busted with #{dealer_score}.  You win!"
     elsif player_score == dealer_score
-      @info = "Dealer and #{session[:player_name]} tie.  Push!"
+      @info = "Dealer and #{session[:player_name]} tie with #{player_score}.  Push!"
     elsif player_score > dealer_score
-      @success = "#{session[:player_name]} wins!"
+      @success = "#{session[:player_name]} wins with #{player_score}!"
     else
-      @error = "Dealer wins!"
+      @error = "Dealer wins with #{dealer_score}!"
     end
   end
 
   def blackjack?
+    # It is only a Blackjack! if it is 21 with two cards, otherwise it is simply a score of 21.
+    if session[:player_cards].count > 2 || session[:dealer_cards].count >2
+      return false
+    end
+
     player_score = hand_value(session[:player_cards])
     dealer_score = hand_value(session[:dealer_cards])
 
-    if player_score == 21 && dealer_score == 21
+    if player_score == BLACKJACK_AMOUNT && dealer_score == BLACKJACK_AMOUNT
       @info = "Dealer and #{session[:player_name]} both got Blackjack!  Push!"
       true
-    elsif player_score == 21
+    elsif player_score == BLACKJACK_AMOUNT
       @success = "#{session[:player_name]} got Blackjack!  You win!"
       true
-    elsif dealer_score == 21
+    elsif dealer_score == BLACKJACK_AMOUNT
       @error = "Dealer got Blackjack.  You lose!"
       true
     else
       false
     end
   end
-
-  # Moved dealer turn logic from POST dealer_next_card to Helper to allow direct calling from player_stand POST method.
-  def dealer_turn
-    if hand_value(session[:dealer_cards]) < 17
-      session[:dealer_cards] << session[:deck].pop
-    end
-
-    if hand_value(session[:dealer_cards]) >= 17 && hand_value(session[:dealer_cards]) <= 21
-      session[:game_over?] = true
-      get_winner_message
-      halt erb(:game)
-    elsif hand_value(session[:dealer_cards]) > 21
-      session[:dealer_bust?] = true
-      session[:game_over?] = true
-      get_winner_message
-      halt erb(:game)
-    end
-
-    erb :game
-  end
-
 end
 
 #####################################################
@@ -120,12 +110,9 @@ end
 #####################################################
 before do
   @show_hit_and_stand_buttons = true
-
-  if session[:game_over?] == true
-    get_winner_message
-  elsif session[:player_stand?] == true && session[:initialized] == true
-    @success = "#{session[:player_name]} is standing."
-  end
+  @show_dealer_hit_button = false
+  @show_dealer_hand = false
+  @show_play_again_button = false
 end
 
 #####################################################
@@ -137,7 +124,7 @@ get '/' do
   if session[:initialized == true]
     erb :game
   else
-    redirect :set_player
+    redirect '/set_player'
   end
 end
 
@@ -153,12 +140,6 @@ get '/game' do
     suits = ['H', 'D', 'C', 'S']
     values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
     session[:deck] = suits.product(values).shuffle! #[ ['H', '2'], ['H', '3'], ...etc]
-    
-    # Initialize tracking variables
-    session[:player_stand?] = false
-    session[:player_bust?] = false
-    session[:dealer_bust?] = false
-    session[:game_over?] = false
 
     # Deal initial cards
     session[:dealer_cards] = []
@@ -169,11 +150,39 @@ get '/game' do
     session[:player_cards] << session[:deck].pop
 
     if blackjack?
-      session[:game_over?] = true
-      halt erb(:game)
+      redirect '/game/compare'
     end
 
     session[:initialized] = true
+  end
+
+  erb :game
+end
+
+get '/game/dealer' do
+  @show_hit_and_stand_buttons = false
+  @show_dealer_hand = true
+
+  dealer_total = hand_value(session[:dealer_cards])
+
+  if dealer_total > BLACKJACK_AMOUNT
+    redirect '/game/compare'
+  elsif dealer_total >= DEALER_MINIMUM
+    redirect '/game/compare'
+  else
+    @show_dealer_hit_button = true
+  end
+
+  erb :game
+end
+
+get '/game/compare' do
+  @show_hit_and_stand_buttons = false
+  @show_dealer_hand = true
+  @show_play_again_button = true
+
+  if blackjack? == false
+    get_winner_message
   end
 
   erb :game
@@ -190,45 +199,46 @@ post '/new_player' do
   if params[:player_name].empty?
     @error = "Name is required.  Please enter a name."
     halt erb(:new_user_form)
+  elsif params[:player_name].match(/\d/)
+    @error = "Digits are not allowed.  Please enter a name without digits."
+    halt erb(:new_user_form)
   end
 
   session[:player_name] = params[:player_name]
-  redirect :game
+  redirect '/game'
 end
 
-post '/player_hit' do
+post '/player/hit' do
   session[:player_cards] << session[:deck].pop
 
-  if hand_value(session[:player_cards]) > 21
-    session[:game_over?] = true
-    session[:player_bust?] = true
-    get_winner_message
-    halt erb(:game)
+  if hand_value(session[:player_cards]) > BLACKJACK_AMOUNT
+    halt redirect '/game/compare'
   end
 
   erb :game
 end
 
-post '/player_stand' do
-  session[:player_stand?] = true
-  @success = "#{session[:player_name]} is standing."
-
-  # Call helper method directly in order to avoid displaying Next Card button when
-  # dealer is not required to hit.
-  dealer_turn   
+post '/player/stand' do
+  @success = "#{session[:player_name]} stands."
+  redirect '/game/dealer'   
 end
 
-post '/dealer_next_card' do
-  dealer_turn
+post '/game/dealer/hit' do
+  session[:dealer_cards] << session[:deck].pop
+  redirect '/game/dealer'
 end
 
 post '/startover' do
-  # Reset trackign variables
-  session[:player_stand?] = false
-  session[:player_bust?] = false
-  session[:dealer_bust?] = false
-  session[:game_over?] = false
   session[:initialized] = false
+  redirect '/set_player'
+end
 
-  redirect :set_player
+post '/play_again' do
+  session[:initialized] = false
+  redirect '/game'
+end
+
+post '/game_over' do
+  session[:initialized] = false
+  erb :game_over
 end
